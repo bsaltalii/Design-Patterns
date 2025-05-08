78de03e4b5364080727e9b6fd9c2a73d3824bddb
Burak SALTALI <bsaltali26@gmail.com>
2025-05-08 11:24:51 +0300

    Update VicePresident.java

--- Design Patterns/ChainOfResponsibilityDesignPattern/VicePresident.java
+++ Design Patterns/ChainOfResponsibilityDesignPattern/VicePresident.java
@@ -2,7 +2,7 @@ package ChainOfResponsibilityDesignPattern;
 
 class VicePresident extends Approver {
     protected boolean canApprove(double amount) {
-        return amount <= 50000;
+        return amount <= 5000;
     }
     protected void approve(double amount) {
         System.out.println("Vice President approved $" + amount);
