c8762a56ad55ba42a0181e713c8211b7f89846a8
Burak SALTALI <bsaltali26@gmail.com>
2025-05-08 11:19:45 +0300

    add : Chain Of Responsibility Pattern

---
+++ Design Patterns/ChainOfResponsibilityDesignPattern/Approver.java
@@ -0,0 +1,22 @@
+package ChainOfResponsibilityDesignPattern;
+
+abstract class Approver {
+    protected Approver next;
+
+    public void setNext(Approver next) {
+        this.next = next;
+    }
+
+    public void handleRequest(double amount) {
+        if (canApprove(amount)) {
+            approve(amount);
+        } else if (next != null) {
+            next.handleRequest(amount);
+        } else {
+            System.out.println("No one can approve this amount!");
+        }
+    }
+
+    protected abstract boolean canApprove(double amount);
+    protected abstract void approve(double amount);
+}
---
+++ Design Patterns/ChainOfResponsibilityDesignPattern/Main.java
@@ -0,0 +1,19 @@
+package ChainOfResponsibilityDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Approver manager = new Manager();
+        Approver director = new Director();
+        Approver vp = new VicePresident();
+        Approver president = new President();
+
+        manager.setNext(director);
+        director.setNext(vp);
+        vp.setNext(president);
+
+        manager.handleRequest(500);
+        manager.handleRequest(8000);
+        manager.handleRequest(20000);
+        manager.handleRequest(100000);
+    }
+}
