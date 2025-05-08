a4849c0e06cb0014d5be1f7e979033e42518d3e6
Burak SALTALI <bsaltali26@gmail.com>
2025-05-08 10:15:29 +0300

    add : flyweight pattern

---
+++ Design Patterns/FlyweightDesingPattern/TreeExample/TreeFactory.java
@@ -0,0 +1,16 @@
+package FlyweightDesingPattern.TreeExample;
+import java.util.HashMap;
+
+class TreeFactory {
+    static HashMap<String, TreeType> types = new HashMap<>();
+
+    public static TreeType getTreeType(String name, String color, String texture) {
+        String key = name + "-" + color + "-" + texture;
+        if (!types.containsKey(key)) {
+            types.put(key, new TreeType(name, color, texture));
+            System.out.println("New TreeType created: " + key);
+        }
+        return types.get(key);
+    }
+}
+
---
+++ Design Patterns/FlyweightDesingPattern/TreeExample/TreeType.java
@@ -0,0 +1,16 @@
+package FlyweightDesingPattern.TreeExample;
+class TreeType {
+    private final String name;
+    private final String color;
+    private final String texture;
+
+    public TreeType(String name, String color, String texture) {
+        this.name = name;
+        this.color = color;
+        this.texture = texture;
+    }
+
+    public void draw(int x, int y) {
+        System.out.println("Drawn Tree → Type: " + name + ", Location: (" + x + ", " + y + ")");
+    }
+}
